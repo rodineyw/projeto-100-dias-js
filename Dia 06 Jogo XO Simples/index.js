50f46746191f4a5587e3466d99dfdeabfdbf4227
rodineyw <rodineyw@yahoo.com.br>
2023-05-10 21:36:08 -0300

    script

--- Dia 06 Jogo XO Simples/index.js
+++ Dia 06 Jogo XO Simples/index.js
@@ -1,10 +1,9 @@
-var playerTurn. moves, isGameOver, span, restartButton;
+var playerTurn, moves, isGameOver, span, restartButton;
 playerTurn = "x";
 moves = 0;
 isGameOver = false;
-span =document.getElementsByTagName("span");
+span = document.getElementsByTagName("span");
 restartButton = '<button onclick="playAgain()"><svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-arrow-repeat" viewBox="0 0 16 16"><path d="M11.534 7h3.932a.25.25 0 0 1 .192.41l-1.966 2.36a.25.25 0 0 1-.384 0l-1.966-2.36a.25.25 0 0 1 .192-.41zm-11 2h3.932a.25.25 0 0 0 .192-.41L2.692 6.23a.25.25 0 0 0-.384 0L.342 8.59A.25.25 0 0 0 .534 9z"/><path fill-rule="evenodd" d="M8 3c-1.552 0-2.94.707-3.857 1.818a.5.5 0 1 1-.771-.636A6.002 6.002 0 0 1 13.917 7H12.9A5.002 5.002 0 0 0 8 3zM3.1 9a5.002 5.002 0 0 0 8.757 2.182.5.5 0 1 1 .771.636A6.002 6.002 0 0 1 2.083 9H3.1z"/></svg></button>';
-
 
 function play(y) {
     if (y.dataset.player == "none" && window.isGameOver == false) {
@@ -17,6 +16,7 @@ function play(y) {
             playerTurn = "x";
         }
     }
+    
     /* Tipos de vitorias */
 
     checkWinner(1, 2, 3);
@@ -42,7 +42,7 @@ function checkWinner(a, b, c) {
     if ((span[a].dataset.player === span[b].dataset) && (span[b].dataset.player === span[c].dataset.player) && (span[a].dataset.player === span[c].dataset.player) && (span[a].dataset.player === "x" || span[a].dataset.player === "o") && isGameOver == false) {
         span[a].parentNode.className += " activeBox ";
         span[b].parentNode.className += " activeBox ";
-        span[x].parentNode.className += " activeBox ";
+        span[c].parentNode.className += " activeBox ";
         gameOver(a);
     }
 }
@@ -56,4 +56,30 @@ function playAgain() {
     }
 }
 
-function 
+function resetGame() {
+    for (i = 0; i < span.length; i++) {
+        span[i].dataset.player = "none";
+        span[i].innerHTML = "&nbsp;";
+    }
+    playerTurn = "x";
+}
+
+function gameOver(a) {
+    var gameOverAlertElement = "<b>GAME OVER </b><br><br> Player " + span[a].dataset.player.tpUpperCase() + ' Win !!! <br><br>' + restartButton;
+    var div = document.createElement("div");
+    div.className = "alert";
+    div.innerHTML = gameOverAlertElement;
+    document.getElementsByTagName("body")[0].appendChild(div);
+    window.isGameOver = true;
+    moves = 0;
+}
+
+function draw() {
+    var drawAlertElement = '<b>DRAW!!!</b><br><br>' + restartButton;
+    var div = document.createElement("div");
+    div.className = "alert";
+    div.innerHTML = drawAlertElement;
+    document.getElementsByTagName("body")[0].appendChild(div);
+    window.isGameOver = true;
+    moves = 0;
+}
